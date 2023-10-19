26d6a06e01e1d5526164e13fad9ec5613a2881a4
Rauls Valgis <raulsvalgis68@gmail.com>
2023-10-19 15:54:30 +0300

    calendar & rates

--- resources/views/currency/index.blade.php
+++ resources/views/currency/index.blade.php
@@ -1,6 +1,10 @@
 @extends('auth.layouts')
 
 @section('content')
+
+    <link href="https://cdn.jsdelivr.net/npm/fullcalendar@5.5.0/main.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/fullcalendar@5.5.0/main.min.js"></script>
+
 <style>
     #currency-chart-container {
         width: 100%; /* Adjust width as needed */
@@ -17,6 +21,10 @@
         width: 90%;
         height: 90%;
     }
+
+    #calendar {
+        width: 100%
+    }
 </style>
 
 <div id="currency-chart-container">
@@ -25,12 +33,223 @@
 <div id="currency-line-chart-container">
     <canvas id="currency-line-chart"></canvas>
 </div>
+<div class="row">
+    <div class="col-6">
+        <div id="calendar"></div>
+    </div>
+    <div class="col-6">
+        <div class="form-group">
+            <label for="startDateInput">{{ __('Start Date:') }}</label>
+            <input type="date" id="startDateInput" class="form-control">
+        </div>
+        <div class="form-group">
+            <label for="endDateInput">{{ __('End Date:') }}</label>
+            <input type="date" id="endDateInput" class="form-control">
+        </div>
+        <button type="button" class="btn btn-primary" onclick="FetchCustomTimeframe()">{{ __('Submit') }}</button>
+        <br>
+        <br>
+            {{ __('Sort By Last:') }}<br>
+        <button type="button" class="btn btn-primary" onclick="FetchDay()" >{{ __('Day') }}</button>
+        <button type="button" class="btn btn-primary" onclick="FetchWeek()" >{{ __('Week') }}</button>
+        <button type="button" class="btn btn-primary" onclick="FetchMonth()" >{{ __('Month') }}</button>
+        <button type="button" class="btn btn-primary" onclick="FetchYear()" >{{ __('Year') }}</button>
+        <br>
+        <br>
+        <div id="calendar-info"></div>
+    </div>
+</div>
 
-<script>
+@include('partials.break')
+
+<script >
+
+
+    var startDate;
+    var endDate;
+    var calendar;
+    var calendarEl;
+
+    document.addEventListener('DOMContentLoaded', function() {
+        calendarEl = document.getElementById('calendar');
+
+        calendar = new FullCalendar.Calendar(calendarEl, {
+            initialView: 'dayGridMonth',
+            events: []
+        });
+
+        calendar.render();
+        
+        // Call FetchYear after initializing the calendar
+        FetchYear();
+    });
 
 
 
 
+
+
+
+
+    
+
+
+
+    function FetchCustomTimeframe() {
+        var startInput = document.getElementById('startDateInput');
+        var endInput = document.getElementById('endDateInput');
+
+        startDate = startInput.value;
+        endDate = endInput.value;
+
+        fetchLineChartData(startDate, endDate);
+    }
+
+
+
+
+
+
+
+
+
+    function FetchDay() {
+        var yesterday = new Date();
+        yesterday.setDate(yesterday.getDate() - 1);
+        var yyyy = yesterday.getFullYear();
+        var mm = String(yesterday.getMonth() + 1).padStart(2, '0');
+        var dd = String(yesterday.getDate()).padStart(2, '0');
+        startDate = endDate = `${yyyy}-${mm}-${dd}`;
+
+        const conversionUrl = `https://currency-conversion-and-exchange-rates.p.rapidapi.com/timeseries?start_date=${startDate}&end_date=${endDate}&from=USD&to=EUR`;
+
+        calendar.destroy();
+
+        // Reinitialize the calendar with the new dates
+        calendar = new FullCalendar.Calendar(calendarEl, {
+            initialView: 'dayGridDay', // Set the view to dayGridDay
+            defaultDate: startDate, // Set the default date to the selected day
+            events: []
+        });
+
+        calendar.render();
+
+        fetchLineChartData(startDate, endDate);
+    }
+
+
+
+
+    function FetchWeek() {
+        var yesterday = new Date();
+        yesterday.setDate(yesterday.getDate() - 1); // Set yesterday as the end date
+
+        var startDate = new Date(yesterday);
+        startDate.setDate(startDate.getDate() - 6); // Calculate the start date as 6 days before yesterday
+
+        var yyyyStart = startDate.getFullYear();
+        var mmStart = String(startDate.getMonth() + 1).padStart(2, '0');
+        var ddStart = String(startDate.getDate()).padStart(2, '0');
+
+        var yyyyEnd = yesterday.getFullYear();
+        var mmEnd = String(yesterday.getMonth() + 1).padStart(2, '0');
+        var ddEnd = String(yesterday.getDate()).padStart(2, '0');
+
+        startDate = `${yyyyStart}-${mmStart}-${ddStart}`;
+        var endDate = `${yyyyEnd}-${mmEnd}-${ddEnd}`; 
+
+        calendar.destroy();
+
+        // Reinitialize the calendar with the new dates
+        calendar = new FullCalendar.Calendar(calendarEl, {
+            initialView: 'dayGridMonth', // Set the view to timeGridWeek for one week
+            initialDate: startDate, // Set the initial date to the start of the week
+            events: []
+        });
+
+        calendar.render();
+
+        // Fetch chart data for the week by passing the startDate and endDate as arguments
+        fetchLineChartData(startDate, endDate);
+    }
+
+
+
+
+
+
+
+    function FetchMonth() {
+        var yesterday = new Date();
+        yesterday.setDate(yesterday.getDate() - 1);
+
+        var startDate = new Date(yesterday);
+        startDate.setDate(startDate.getDate() - 30);
+
+        var yyyyStart = startDate.getFullYear();
+        var mmStart = String(startDate.getMonth() + 1).padStart(2, '0');
+        var ddStart = String(startDate.getDate()).padStart(2, '0');
+
+        var yyyyEnd = yesterday.getFullYear();
+        var mmEnd = String(yesterday.getMonth() + 1).padStart(2, '0');
+        var ddEnd = String(yesterday.getDate()).padStart(2, '0');
+
+        startDate = `${yyyyStart}-${mmStart}-${ddStart}`;
+        var endDate = `${yyyyEnd}-${mmEnd}-${ddEnd}`; 
+
+        calendar.destroy();
+
+        calendar = new FullCalendar.Calendar(calendarEl, {
+            initialView: 'dayGridMonth',
+            initialDate: startDate,
+            events: []
+        });
+
+        calendar.render();
+
+        fetchLineChartData(startDate, endDate);
+    }
+
+
+
+
+
+
+
+
+
+    
+    function FetchYear() {
+        var yesterday = new Date();
+        yesterday.setDate(yesterday.getDate() - 1); 
+
+        var startDate = new Date(yesterday);
+        startDate.setDate(startDate.getDate() - 365); 
+
+        var yyyyStart = startDate.getFullYear();
+        var mmStart = String(startDate.getMonth() + 1).padStart(2, '0');
+        var ddStart = String(startDate.getDate()).padStart(2, '0');
+
+        var yyyyEnd = yesterday.getFullYear();
+        var mmEnd = String(yesterday.getMonth() + 1).padStart(2, '0');
+        var ddEnd = String(yesterday.getDate()).padStart(2, '0');
+
+        startDate = `${yyyyStart}-${mmStart}-${ddStart}`;
+        var endDate = `${yyyyEnd}-${mmEnd}-${ddEnd}`; 
+
+        calendar.destroy();
+
+        calendar = new FullCalendar.Calendar(calendarEl, {
+            initialView: 'dayGridMonth',
+            initialDate: startDate,
+            events: []
+        });
+
+        calendar.render();
+
+        // Fetch chart data for the week by passing the startDate and endDate as arguments
+        fetchLineChartData(startDate, endDate);
+    }
 
 
 
@@ -159,10 +378,7 @@
 
 
 
-    async function fetchLineChartData() {
-        const startDate = '2019-01-01';
-        const endDate = '2019-12-31';
-
+    async function fetchLineChartData(startDate, endDate) {
         const conversionUrl = `https://currency-conversion-and-exchange-rates.p.rapidapi.com/timeseries?start_date=${startDate}&end_date=${endDate}&from=USD&to=EUR`;
 
         const conversionOptions = {
@@ -174,78 +390,90 @@
         };
 
         try {
+            // Destroy the existing line chart if it exists
+            if (lineChart) {
+                lineChart.destroy();
+            }
             const lineChartResponse = await fetch(conversionUrl, conversionOptions);
             const lineChartResult = await lineChartResponse.json();
 
-            const dates = Object.keys(lineChartResult.rates);
-            const values = Object.values(lineChartResult.rates);
+            if (lineChartResult.rates) {
+                // Convert rates data to an array of objects
+                const originalData = Object.entries(lineChartResult.rates).map(([date, rates]) => ({
+                    date,
+                    value: rates.USD 
+                }));
 
-            console.log('dates ',dates);
-            console.log('values ',values)
+                // Create a sorted copy of the data
+                const data = [...originalData];
+                data.sort((a, b) => new Date(a.date) - new Date(b.date));
 
-            const data = dates.map((date, index) => ({
-                date: date,
-                value: values[index]
-            }));
+                const dates = data.map(item => item.date);
+                const values = data.map(item => item.value);
 
-            const lineChartCtx = document.getElementById('currency-line-chart').getContext('2d');
+                const lineChartCtx = document.getElementById('currency-line-chart').getContext('2d');
 
-            const lineChart = new Chart(lineChartCtx, {
-                type: 'line',
-                data: {
-                    labels: data.map(item => item.date),
-                    datasets: [{
-                        label: 'Values',
-                        data: data.map(item => item.value),
-                        backgroundColor: 'rgba(1, 192, 192, 1)',
-                        borderColor: 'rgba(1, 192, 192, 1)',
-                        borderWidth: 1
-                    }]
-                },
-                options: {
-                    responsive: true,
-                    scales: {
-                        x: {
-                            title: {
-                                display: true,
-                                text: 'Time Period'
-                            }
-                        },
-                        y: {
-                            title: {
-                                display: true,
-                                text: 'Rates'
-                            },
-                        }
+                // Use the existing lineChart variable (not redefined)
+                lineChart = new Chart(lineChartCtx, {
+                    type: 'line',
+                    data: {
+                        labels: dates, 
+                        datasets: [{
+                            label: 'USD to EUR Exchange Rate',
+                            data: values, 
+                            backgroundColor: 'rgba(1, 192, 192, 0.2)',
+                            borderColor: 'rgba(1, 192, 192, 1)',
+                            borderWidth: 1,
+                            fill: true,
+                        }]
                     },
-                    plugins: {
-                        legend: {
-                            display: true,
-                            position: 'top',
-                            labels: {
-                                fontColor: 'black',
-                                fontSize: 12,
-                                boxWidth: 15
+                    options: {
+                        responsive: true,
+                        scales: {
+                            x: {
+                                title: {
+                                    display: true,
+                                    text: 'Time Period'
+                                }
+                            },
+                            y: {
+                                title: {
+                                    display: true,
+                                    text: '1 EUR to USD'
+                                },
                             }
                         },
-                        tooltip: {
-                            callbacks: {
-                                label: (context) => {
-                                    const item = data[context.dataIndex];
-                                    return `Date: ${item.date}, Value: ${item.value}`;
+                        plugins: {
+                            legend: {
+                                display: true,
+                                position: 'top',
+                                labels: {
+                                    fontColor: 'black',
+                                    fontSize: 12,
+                                    boxWidth: 15
+                                }
+                            },
+                            tooltip: {
+                                callbacks: {
+                                    label: (context) => {
+                                        const item = originalData[context.dataIndex];
+                                        return `Date: ${item.date}, Value: ${item.value}`;
+                                    }
                                 }
                             }
                         }
-                    },
-                }
-            });
+                    }
+                });
+            } else {
+                console.error('No exchange rate data found in the API response.');
+            }
         } catch (error) {
             console.error('Error fetching or rendering chart:', error);
         }
     }
 
-// Call the function to fetch and display the chart
-fetchLineChartData();
+fetchLineChartData(startDate, endDate);
+
 
 
 
